0d15709a48dd3098d81b159735604f5f94d44589
vortexai18 <137415826+vortexai18@users.noreply.github.com>
2023-06-24 11:20:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,28 @@
-from flask import Flask
-app = Flask(__name__)
+from langchain.agents import create_csv_agent
+from langchain.llms import OpenAI
+from dotenv import load_dotenv
+import os
+from flask import Flask, render_template, request, jsonify
 
+app = Flask(__name__)
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    return "Hello World"
+
+@app.route('/query', methods=['POST'])
+def query():
+    load_dotenv()
+    if os.getenv("OPENAI_API_KEY") is None or os.getenv("OPENAI_API_KEY") == "":
+        return jsonify({'error': 'OpenAI Key is not set'}), 500
+
+    agent = create_csv_agent(OpenAI(temperature=0), "/Users/shreyas/Downloads/AAPL.csv")
+    user_question = request.form.get('query')
+
+    if user_question is None or user_question == "":
+        return jsonify({'error': 'No query provided'}), 400
+
+    ans = agent.run(user_question)
+    return jsonify({'answer': ans}), 200
+
+if __name__ == '__main__':
+    app.run()
